f3ae00dc77ab8574f3c1e142b55fe6c845ff546f
MohomedThariq <1234thariq@gmail.com>
2022-04-01 19:08:16 +0530

    f

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from pickle import FALSE
 from flask import Flask, jsonify, request
 import json
 
@@ -20,4 +21,4 @@ def pred():
 
 
 if __name__ == "__main__":
-    app.run(debug = True, host = '0.0.0.0')
+    app.run(debug = False, host = '0.0.0.0')
