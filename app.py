e3334f619b0e68bbb9d74d03728771bde60767e2
MohomedThariq <1234thariq@gmail.com>
2022-04-01 19:56:37 +0530

    ne

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from pickle import FALSE
+from pickle
+import os
 from flask import Flask, jsonify, request
 import json
 
@@ -21,4 +22,5 @@ def pred():
 
 
 if __name__ == "__main__":
-    app.run(debug = False, host = '0.0.0.0')
+    port = os.environ.get("PORT", 5000)
+    app.run(debug = False, host = '0.0.0.0', port=port)
