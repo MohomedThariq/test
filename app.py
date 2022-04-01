23f79946fd943b5a7bafa6c64b0bf181694c221c
MohomedThariq <1234thariq@gmail.com>
2022-04-01 21:04:59 +0530

    t

--- app.py
+++ app.py
@@ -1,28 +1,48 @@
 import os
+from distutils.log import debug
 from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+from check.checker import PredictionAPI
+from flask_cors import CORS
+# from resources.routes import initialize_routes
 import json
 
-app = Flask(__name__)
 
-@app.route('/', methods = ['GET'])
-def hi():
-    return jsonify({'hello': 'world'})
+app=Flask(__name__)
+api=Api(app)
 
-@app.route('/get', methods = ['GET'])
-def hello():
-    return jsonify({'hello': 'world'})
 
-@app.route('/pred', methods = ['POST'])
-def pred():
-    body = request.json['body']
+# class CoreApi(Resource):
+@app.route('/')
+def home():
+    return "UrHeart"
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
     test_data = request.json
     s1 = json.dumps(test_data)
     print_json = json.loads(s1)
     list = print_json['body']
-    input_data = (float(list['age']), float(list['sex']), float(list['chestPainType']),	float(list['restingBP']), float(list['cholestrol']), float(list['fastingBloodSugar']), float(list['restingECG']), float(list['maxHeartRate']), float(list['exerciseAngina']),	float(list['oldpeak']), float(list['STslope']))
-    print(input_data)
-    return jsonify({'predResult':'1'})
+    input_data = (  float(list['age']), 
+                    float(list['sex']), 
+                    float(list['chestPainType']),	
+                    float(list['restingBP']), 
+                    float(list['cholestrol']), 
+                    float(list['fastingBloodSugar']), 
+                    float(list['restingECG']), 
+                    float(list['maxHeartRate']), 
+                    float(list['exerciseAngina']),	
+                    float(list['oldpeak']), 
+                    float(list['STslope']))
+    return jsonify({'predResult':str(PredictionAPI(input_data))})
 
+
+@app.route('/test')
+def test():
+    return 'test'
+
+# initialize_routes(api)
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", 5000)
